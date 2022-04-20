cf85b76634082dd6a3797721317a4df74e63f87f
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-04-20 11:20:38 +1000

    functioning ButtonGroup

--- src/components/buttonGroup.jsx
+++ src/components/buttonGroup.jsx
@@ -1,7 +1,42 @@
-const ButtonGroup = () => {
-    return (
-        <button>button</button>
-    )
-}
+import { useState } from "react";
+import styled from "styled-components";
 
-export default ButtonGroup
+const ButtonGroup = ({ buttons }) => {
+  const [clickedId, setClickedId] = useState(-1);
+
+  const NumBtnsDiv = styled.div`
+    display: flex;
+    justify-content: space-around;
+  `;
+  const NumBtn = styled.button`
+  color: hsl(0, 0%, 100%);
+    background-color: ${(props) => (props.background ? "hsl(217, 12%, 63%)" : 'hsl(213, 19%, 18%)')};
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+  border-style: none;
+
+  :hover {
+    transition:0.25s;
+    background-color: hsl(25, 97%, 53%);
+    `;
+
+  return (
+    <NumBtnsDiv>
+      {buttons.map((buttonLabel, i) => {
+        return (
+          <NumBtn
+            key={i}
+            name={buttonLabel}
+            onClick={() => setClickedId(i)}
+            background={i === clickedId ? true : false}
+          >
+            {buttonLabel}
+          </NumBtn>
+        );
+      })}
+    </NumBtnsDiv>
+  );
+};
+
+export default ButtonGroup;
--- src/components/rating-component.jsx
+++ src/components/rating-component.jsx
@@ -17,23 +17,7 @@ const Text = styled.p`
   font-size: 15px;
   color: white;
 `;
-const NumBtnsDiv = styled.div`
-  display: flex;
-  justify-content: space-around;
-`;
-const NumBtn = styled.button`
-  color: hsl(0, 0%, 100%);
-  background-color: hsl(213, 19%, 18%);
-  width: 45px;
-  height: 45px;
-  border-radius: 50%;
-  border-style: none;
 
-  :hover {
-    transition:0.25s;
-    background-color: hsl(25, 97%, 53%);
-  }
-`;
 const SubmitDiv = styled.div`
   display: flex;
   justify-content: center;
@@ -54,49 +38,7 @@ const SubmitButton = styled.button`
 `;
 
 const RatingComponent = () => {
-  const [btn1, setBtn1] = useState(false);
-  const [btn2, setBtn2] = useState(false);
-  const [btn3, setBtn3] = useState(false);
-  const [btn4, setBtn4] = useState(false);
-  const [btn5, setBtn5] = useState(false);
 
-  const clickBtn1 = () => {
-    setBtn1(true);
-    setBtn2(false)
-    setBtn3(false)
-    setBtn4(false)
-    setBtn5(false)
-  };
-  const clickBtn2 = () => {
-    setBtn1(false);
-    setBtn2(true)
-    setBtn3(false)
-    setBtn4(false)
-    setBtn5(false)
-  };
-  const clickBtn3 = () => {
-    setBtn1(false);
-    setBtn2(false)
-    setBtn3(true)
-    setBtn4(false)
-    setBtn5(false)
-  };
-  const clickBtn4 = () => {
-    setBtn1(false);
-    setBtn2(false)
-    setBtn3(false)
-    setBtn4(true)
-    setBtn5(false)
-  };
-  const clickBtn5 = () => {
-    setBtn1(false);
-    setBtn2(false)
-    setBtn3(false)
-    setBtn4(false)
-    setBtn5(true)
-  };
-
-  const handleSubmit = () => {};
   return (
     <>
       <Container>
@@ -105,7 +47,7 @@ const RatingComponent = () => {
           Please let is know how we did with your support request. All feedback
           is appreciated to help us improve our affering!
         </Text>
-        <ButtonGroup />
+        <ButtonGroup buttons={["1", "2", "3", "4", "5"]} />
         <SubmitDiv>
           <SubmitButton type="submit">SUBMIT</SubmitButton>
         </SubmitDiv>
