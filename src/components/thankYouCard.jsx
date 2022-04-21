ac0dc0866d6e52f2331e1f623984b65686be1181
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-04-21 15:11:48 +1000

    more work on the Thank You card

--- src/components/thankYouCard.jsx
+++ src/components/thankYouCard.jsx
@@ -1,7 +1,29 @@
-const ThankYouCard = () => {
-    return (
-        <h1>Thank you</h1>
-    )
-}
+import { useContext } from "react";
+import AppContext from "../context/AppContext";
 
-export default ThankYouCard
+import { useNavigate } from "react-router-dom";
+
+const ThankYouCard = () => {
+  const { context } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  let score = context.activeBtn + 1;
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <>
+      <p>You selected {score} out of 5</p>
+      <h1>Thank you</h1>
+      <p>
+        We appreciate you taking the time to give a rating. If you ever need
+        more support, don't hesitate to get in touch!
+      </p>
+      <button onClick={handleClick}>back</button>
+    </>
+  );
+};
+
+export default ThankYouCard;
